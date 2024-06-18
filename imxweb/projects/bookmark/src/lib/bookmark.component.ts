e2bca68ed8fb21b36d0c862710b875cbaacf22ea
Russo Gilles - Admin <AGILLESR@oneim-demo.local>
2024-06-18 16:36:58 +0200

    Started overhauling component logic

--- imxweb/projects/bookmark/src/lib/bookmark.component.ts
+++ imxweb/projects/bookmark/src/lib/bookmark.component.ts
@@ -3,6 +3,8 @@ import { BookmarkService } from './bookmark.service';
 import { QerApiService } from 'qer';
 import { imx_SessionService } from 'qbm';
 import { PortalPersonMasterdata } from 'imx-api-qer';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'imx-bookmark',
@@ -15,28 +17,64 @@ export class BookmarkComponent implements OnInit {
   public portalPersonAdmin: any;
   public portalPerson: PortalPersonMasterdata;
   existingLinks: string[] = [];
+  included: boolean;
+  iconValue: string;
 
   constructor(
     private bookmarkService: BookmarkService,
     private readonly sessionService: imx_SessionService,
     private qerClient: QerApiService,
+    private router: Router
   ) {
   }
 
   public async addRouterLink() {
     this.existingLinks = this.portalPersonAdmin.Data[0].GetEntity().GetColumn("CustomProperty09").GetValue(JSON.parse);
     const currentLink = this.bookmarkService.getCurrentRoute();
-    this.bookmarkService.saveLink(currentLink, this.existingLinks);
     console.log('Updated Router Links Array:', this.existingLinks);
+    let url = this.bookmarkService.getCurrentRoute();
+    this.included = this.bookmarkService.checkState(url, this.existingLinks);
+    if (this.included = false) {
+      this.iconValue = "openbookmark"
+    }
+    if (this.included = true) {
+      this.iconValue = "bookmark"
+    }
+    this.bookmarkService.saveLink(currentLink, this.existingLinks, this.included, this.iconValue);
     const links = this.bookmarkService.getLinksArray();
-
     this.portalPersonAdmin.Data[0].GetEntity().GetColumn("CustomProperty09").PutValue(JSON.stringify(links));
     this.portalPersonAdmin.Data[0].GetEntity().Commit(true);
   }
 
   async ngOnInit(): Promise<void> {
+    
+    console.log(this.included);
     this.userId = (await this.sessionService.getSessionState()).UserUid;
-    this.portalPersonAdmin = (await this.qerClient.typedClient.PortalPersonMasterdataInteractive.Get_byid(this.userId))
+    this.portalPersonAdmin = (await this.qerClient.typedClient.PortalPersonMasterdataInteractive.Get_byid(this.userId));
+    this.existingLinks = this.portalPersonAdmin.Data[0].GetEntity().GetColumn("CustomProperty09").GetValue(JSON.parse);
+    let url = this.bookmarkService.getCurrentRoute();
+    this.included = this.bookmarkService.checkState(url, this.existingLinks);
+    if (this.included = false) {
+      this.iconValue = "openbookmark"
+    }
+    if (this.included = true) {
+      this.iconValue = "bookmark"
+    }
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      console.log('Route changed to:', event.urlAfterRedirects);
+      let url = this.bookmarkService.getCurrentRoute();
+      this.included = this.bookmarkService.checkState(url, this.existingLinks);
+      if (this.included = false) {
+        this.iconValue = "openbookmark"
+      }
+      if (this.included = true) {
+        this.iconValue = "bookmark"
+      }
+    });
+
     console.log("Bookmark Component Loaded")
   }
 }
--- imxweb/projects/bookmark/src/lib/bookmark.service.ts
+++ imxweb/projects/bookmark/src/lib/bookmark.service.ts
@@ -8,9 +8,9 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class BookmarkService implements OnInit {
+  included: boolean;
   routerLinks: string[][] = [];
   links: string[] = [];
-  private MAX_URL_LENGTH = 2048;  // Example maximum length, adjust as needed
 
   constructor(
     private readonly extService: ExtService,
@@ -18,7 +18,6 @@ export class BookmarkService implements OnInit {
   ) { }
 
   ngOnInit() {
-
     this.extService.register('Bookmark', { instance: BookmarkComponent })
 
   }
@@ -27,15 +26,37 @@ export class BookmarkService implements OnInit {
     return this.router.url;
   }
 
-  saveLink(url: string, existingLinks: string[]) {
+  checkState(url: string, existingLinks: string[]){
+    console.log("Check State has been called");
+    if(this.links.includes(url) || existingLinks.includes(url)){
+      this.included = true;
+      console.log(this.included);
+      return true
+    }
+    else{
+      this.included = false;
+      console.log(this.included);
+      return false 
+    } 
+  }
+
+  saveLink(url: string, existingLinks: string[], included: boolean, iconValue: String) {
     console.log("This is the Routerlink: ", this.router.url);
     if (!this.links.includes(url) && !existingLinks.includes(url)) {
       this.links.push(url);
       console.log("Item was pushed")
     }
-    console.log("Item was not pushed")
+    if(included === true && iconValue === "bookmark"){
+      for (let i = this.links.length - 1; i >= 0; i--) {
+        if (this.links[i] === url) {
+          this.links.splice(i, 1);
+          console.log("Item was delted");
+        }
+      }
+    }
 
     console.log('Router Links', this.links);
+    
   }
 
   getLinksArray(): string[]{
